f5c5e1eb1d923a151e0584b6c0c6189842c1d140
Nathan Solomon <nbsolomon4559@gmail.com>
2020-09-13 05:28:25 -0500

    adding basics of javascript events

---
+++ eventRequests.md
@@ -0,0 +1,82 @@
+# Event Requests
+This documentation outlines the structure of event requests and how they can be interacted with
+
+# General
+Callbacks are currently not implemented yet. Additionally, all scripts currently call one php file called `events.php`. This can easily be changed to be either a different file name or multiple files for different events
+
+## Move Person Requests
+This event is designed to move a person from one location to another. The `personId` should move the person with that id from their current location to the tile of the `destinationId`
+```
+Format: GET
+```
+Fields
+```
+classroomId: int
+personId: int
+destinationId: int
+```
+Example Request
+```
+events.php?classroomId=3&personId=4&destinationId=7
+```
+
+## Update Person State
+This event is designed to update the state of a person. This can be used to control if a person is wearing a face mask or face shield. The `personId` should update the person to be wearing or not wearing the `mask` and `faceshield`
+```
+Format: GET
+```
+Fields
+```
+classroomId: int
+personId: int
+mask: boolean
+faceshield: boolean
+```
+Example Request
+```
+events.php?classroomId=2&personId=2&mask=true&faceshield=false
+```
+
+## Use Lysol
+This event is designed to indicate that a person of a specific `personId` has used the lysol at their current location
+```
+Format: GET
+```
+Fields
+```
+classroomId: int
+personId: int
+```
+Example Request
+```
+events.php?classroomId=2&personId=5
+```
+
+## Use Hand Sanitizer
+This event is designed to indicate that a person of a specific `personId` has used the lysol at their current location
+```
+Format: GET
+```
+Fields
+```
+classroomId: int
+personId: int
+```
+Example Request
+```
+events.php?classroomId=2&personId=5
+```
+
+## Request Data Update
+This event is designed to request an update of the data model for a specific classroom.
+```
+Format: GET
+```
+Fields
+```
+classroomId: int
+```
+Example Request
+```
+events.php?classroomId=3
+```
--- js/events.js
+++ js/events.js
@@ -1,15 +1,60 @@
+var targetScript = 'events.php?';
+
 function movePerson(formData) {
-    // TODO: implement request
+    request = new XMLHttpRequest();
+    let requestContent = targetScript;
+    requestContent = requestContent+'personId='+formData.movePersonId.value;
+    requestContent = requestContent+'&destinationId='+formData.moveDestinationSquare.value;
+    request.open("GET", requestContent);
+
+    request.onreadystatechange = function () {
+        if (request.readyState == 4);
+            // TODO implement callback
+    }
+
+    request.send();
 }
 
 function updatePersonState(formData) {
-    // TODO: implement request
+    request = new XMLHttpRequest();
+    let requestContent = targetScript;
+    requestContent = requestContent+'personId='+formData.updatePersonId.value;
+    requestContent = requestContent+'&mask='+formData.updateWearingMask.checked;
+    requestContent = requestContent+'&faceshield='+formData.updateWearingFaceshield.checked;
+    request.open("GET", requestContent);
+
+    request.onreadystatechange = function () {
+        if (request.readyState == 4);
+            // TODO implement callback
+    }
+
+    request.send();
 }
 
 function useLysol(formData) {
-    // TODO: implement request
+    request = new XMLHttpeRequest();
+    let requestContent = targetScript;
+    requestContent = requestContent+'personId='+formData.useLysolPersonId.value;
+    request.open("GET", requestContent);
+
+    request.onreadystatechange = function () {
+        if (request.readyState == 4);
+            // TODO implement callback
+    }
+
+    request.send();
 }
 
-function userSanitizer(formData) {
-    // TODO: implement request
+function useSanitizer(formData) {
+    request = new XMLHttpeRequest();
+    let requestContent = targetScript;
+    requestContent = requestContent+'personId='+formData.useSanitizerPersonId.value;
+    request.open("GET", requestContent);
+
+    request.onreadystatechange = function () {
+        if (request.readyState == 4);
+            // TODO implement callback
+    }
+
+    request.send();
 }
