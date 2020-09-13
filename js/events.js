17f5c2e78fc8007325261afd2d71a9f319f75a83
Nathan Solomon <nbsolomon4559@gmail.com>
2020-09-13 04:09:25 -0500

    adding initial User Interface work

---
+++ js/events.js
@@ -0,0 +1,15 @@
+function movePerson(formData) {
+    // TODO: implement request
+}
+
+function updatePersonState(formData) {
+    // TODO: implement request
+}
+
+function useLysol(formData) {
+    // TODO: implement request
+}
+
+function userSanitizer(formData) {
+    // TODO: implement request
+}
