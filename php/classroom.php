e530265aad003fb00096625fe9dbf39a21a1a3af
Jada Blankenship <30299670+jada4569@users.noreply.github.com>
2020-09-14 20:10:35 -0500

    Added occupants as a variable for the classroom object

--- php/classroom.php
+++ php/classroom.php
@@ -3,6 +3,7 @@
 class Classroom {
     // Member variables
     private $rectangles = array();
+    private $occupants = array();
     private $classroomId;
     
     // Member functions
@@ -21,8 +22,18 @@ class Classroom {
         array_push($rectangles, $rectangle);
     }
 
+    // Adds a Person to the Classroom
+    public function addPerson($person) {
+        array_push($occupants, $person);
+    }
+
     // Returns the Classroom's rectangles
     public function getRectangles() {
         return $this->rectangles;
+    }
+
+    // Returns the Classroom's occupants
+    public function getOccupants() {
+        return $this->occupants;
     }
 }
