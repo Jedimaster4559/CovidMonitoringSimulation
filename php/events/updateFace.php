5c0fa8cc88106b61e372960a02d62239e5b28eaa
Jada Blankenship <30299670+jada4569@users.noreply.github.com>
2020-09-17 04:35:33 -0500

    added updateFace logic

--- php/events/updateFace.php
+++ php/events/updateFace.php
@@ -3,125 +3,160 @@ include_once('../classroom.php');
 include_once('../person.php');
 include_once('../rectangle.php');
 
+$class = $_REQUEST["classroomId"];
 $person = $_REQUEST["personId"];
 $mask = $_REQUEST["mask"];
 $faceshield = $_REQUEST["faceshield"];
-$personClass;
-$continue = true;
 
-$c1People = $class1->occupants;
-$c1Count = 0;
-while ($c1Count < count($class1->occupants)) {
-    $myPerson = $class1->occupants[$c1Count];
-    if ($myPerson->personId == $person) {
-        if ($myPerson->status == "student") {
+// Fixes a PHP serialization issue
+// https://tommcfarlin.com/cast-a-php-standard-class-to-a-specific-type/
+function cast($instance, $className) {
+    return unserialize(sprintf('O:%d:"%s"%s', \strlen($className), $className, strstr(strstr(serialize($instance), '"'), ':')));    
+}
+
+if ($class == '1') {
+    $c1Text = file_get_contents('../text/class1.txt');
+    $class1 = cast(json_decode($c1Text), "Classroom");
+    $numPeople = count($class1->occupants);
+    $c1Count = 0;
+
+    while ($c1Count < $numPeople) {
+        $personFromFile = cast($class1->occupants[$c1Count], "Person");
+        if ($person == $personFromFile->personId) {
             if ($mask == false) {
                 $myPerson->wearMaskIncorrectly();
+                $class1->occupants[$c1Count-1] = $myPerson;
+                $jsonClass = json_encode($class1);
+                $c1File = fopen('../text/class1.txt', 'w');
+                fwrite($c1File, $jsonClass);
+                fclose($c1File);
             }
             elseif ($mask == true) {
                 $myPerson->wearMaskCorrectly();
+                $class1->occupants[$c1Count-1] = $myPerson;
+                $jsonClass = json_encode($class1);
+                $c1File = fopen('../text/class1.txt', 'w');
+                fwrite($c1File, $jsonClass);
+                fclose($c1File);
             }
-        }
-        else {
-            if ($mask == false) {
-                $myPerson->wearMaskIncorrectly();
-            }
-            elseif ($mask == true) {
-                $myPerson->wearMaskCorrectly();
-            }
-            if ($faceshield == false) {
-                $myPerson->wearFaceShieldIncorrectly();
-            }
-            elseif ($faceshield == true) {
-                $myPerson->wearCorrectly();
-            }
-        }
-        $personClass = $class1;
-        $continue = false;
-    }
-    $c1Count++;
-}
-
-if ($continue == false) {
-    $jsonClass = json_encode($personClass);
-    echo $jsonClass;
-}
-
-else {
-    $c2People = $class2->occupants;
-    $c2Count = 0;
-    while ($c2Count < count($class2->occupants)) {
-        $myPerson = $class2->occupants[$c1Count];
-        if ($myPerson->personId == $person) {
-            if ($myPerson->status == "student") {
-                if ($mask == false) {
-                    $myPerson->wearMaskIncorrectly();
-                }
-                elseif ($mask == true) {
-                    $myPerson->wearMaskCorrectly();
-                }
-            }
-            else {
-                if ($mask == false) {
-                    $myPerson->wearMaskIncorrectly();
-                }
-                elseif ($mask == true) {
-                    $myPerson->wearMaskCorrectly();
-                }
+            if ($myPerson->status == "teacher") {
                 if ($faceshield == false) {
                     $myPerson->wearFaceShieldIncorrectly();
+                    $class1->occupants[$c1Count-1] = $myPerson;
+                    $jsonClass = json_encode($class1);
+                    $c1File = fopen('../text/class1.txt', 'w');
+                    fwrite($c1File, $jsonClass);
+                    fclose($c1File);
                 }
                 elseif ($faceshield == true) {
-                    $myPerson->wearFaceShieldCorrectly();
+                    $myPerson->wearCorrectly();
+                    $class1->occupants[$c1Count-1] = $myPerson;
+                    $jsonClass = json_encode($class1);
+                    $c1File = fopen('../text/class1.txt', 'w');
+                    fwrite($c1File, $jsonClass);
+                    fclose($c1File);
                 }
             }
-            $personClass = $class2;
-            $continue = false;
+        }
+        $c1Count++;
+    }
+    echo $jsonClass;
+}
+elseif ($class == '2') {
+    $c2Text = file_get_contents('../text/class2.txt');
+    $class2 = cast(json_decode($c2Text), "Classroom");
+    $numPeople = count($class2->occupants);
+    $c2Count = 0;
+
+    while ($c2Count < $numPeople) {
+        $personFromFile = cast($class2->occupants[$c2Count], "Person");
+        if ($person == $personFromFile->personId) {
+            if ($mask == false) {
+                $myPerson->wearMaskIncorrectly();
+                $class2->occupants[$c2Count-1] = $myPerson;
+                $jsonClass = json_encode($class2);
+                $c2File = fopen('../text/class2.txt', 'w');
+                fwrite($c2File, $jsonClass);
+                fclose($c2File);
+            }
+            elseif ($mask == true) {
+                $myPerson->wearMaskCorrectly();
+                $class2->occupants[$c2Count-1] = $myPerson;
+                $jsonClass = json_encode($class2);
+                $c2File = fopen('../text/class2.txt', 'w');
+                fwrite($c2File, $jsonClass);
+                fclose($c2File);
+            }
+            if ($myPerson->status == "teacher") {
+                if ($faceshield == false) {
+                    $myPerson->wearFaceShieldIncorrectly();
+                    $class2->occupants[$c2Count-1] = $myPerson;
+                    $jsonClass = json_encode($class2);
+                    $c2File = fopen('../text/class2.txt', 'w');
+                    fwrite($c2File, $jsonClass);
+                    fclose($c2File);
+                }
+                elseif ($faceshield == true) {
+                    $myPerson->wearCorrectly();
+                    $class2->occupants[$c2Count-1] = $myPerson;
+                    $jsonClass = json_encode($class2);
+                    $c2File = fopen('../text/class2.txt', 'w');
+                    fwrite($c2File, $jsonClass);
+                    fclose($c2File);
+                }
+            }
         }
         $c2Count++;
     }
+    echo $jsonClass;
+}
+else {
+    $c3Text = file_get_contents('../text/class3.txt');
+    $class3 = cast(json_decode($c3Text), "Classroom");
+    $numPeople = count($class3->occupants);
+    $c3Count = 0;
 
-    if ($continue == false) {
-        $jsonClass = json_encode($personClass);
-        echo $jsonClass;
-    }
-
-    else {
-        $c3People = $class3->occupants;
-        $c3Count = 0;
-        while ($c3Count < count($class3->occupants)) {
-            $myPerson = $class3->occupants[$c1Count];
-            if ($myPerson->personId == $person) {
-                if ($myPerson->status == "student") {
-                    if ($mask == false) {
-                        $myPerson->wearMaskIncorrectly();
-                    }
-                    elseif ($mask == true) {
-                        $myPerson->wearMaskCorrectly();
-                    }
-                }
-                else {
-                    if ($mask == false) {
-                        $myPerson->wearMaskIncorrectly();
-                    }
-                    elseif ($mask == true) {
-                        $myPerson->wearMaskCorrectly();
-                    }
-                    if ($faceshield == false) {
-                        $myPerson->wearFaceShieldIncorrectly();
-                    }
-                    elseif ($faceshield == true) {
-                        $myPerson->wearFaceShieldCorrectly();
-                    }
-                }
-                $personClass = $class1;
-                $continue = false;
+    while ($c3Count < $numPeople) {
+        $personFromFile = cast($class3->occupants[$c3Count], "Person");
+        if ($person == $personFromFile->personId) {
+            if ($mask == false) {
+                $myPerson->wearMaskIncorrectly();
+                $class3->occupants[$c3Count-1] = $myPerson;
+                $jsonClass = json_encode($class3);
+                $c3File = fopen('../text/class3.txt', 'w');
+                fwrite($c3File, $jsonClass);
+                fclose($c3File);
             }
-            $c3Count++;
+            elseif ($mask == true) {
+                $myPerson->wearMaskCorrectly();
+                $class3->occupants[$c3Count-1] = $myPerson;
+                $jsonClass = json_encode($class3);
+                $c3File = fopen('../text/class3.txt', 'w');
+                fwrite($c3File, $jsonClass);
+                fclose($c3File);
+            }
+            if ($myPerson->status == "teacher") {
+                if ($faceshield == false) {
+                    $myPerson->wearFaceShieldIncorrectly();
+                    $class3->occupants[$c3Count-1] = $myPerson;
+                    $jsonClass = json_encode($class3);
+                    $c3File = fopen('../text/class3.txt', 'w');
+                    fwrite($c3File, $jsonClass);
+                    fclose($c3File);
+                }
+                elseif ($faceshield == true) {
+                    $myPerson->wearCorrectly();
+                    $class3->occupants[$c3Count-1] = $myPerson;
+                    $jsonClass = json_encode($class3);
+                    $c3File = fopen('../text/class3.txt', 'w');
+                    fwrite($c3File, $jsonClass);
+                    fclose($c3File);
+                }
+            }
         }
-        $jsonClass = json_encode($personClass);
-        echo $jsonClass;
+        $c3Count++;
     }
+    echo $jsonClass;
 }
 
 ?>
--- php/events/useCleaner.php
+++ php/events/useCleaner.php
@@ -6,54 +6,6 @@ include_once('../rectangle.php');
 $person = $_REQUEST["personId"];
 $class = $_REQUEST["classroomId"];
 
-// function decode($class) {
-//     $rec1 = $class->rectangles[0];
-//     $rec2 = $class->rectangles[1];
-//     $rec3 = $class->rectangles[2];
-//     $rec4 = $class->rectangles[3];
-//     $rec5 = $class->rectangles[4];
-//     $rec6 = $class->rectangles[5];
-//     $rec7 = $class->rectangles[6];
-//     $rec8 = $class->rectangles[7];
-//     $rec9 = $class->rectangles[8];
-//     $rec10 = $class->rectangles[9];
-//     $rec11 = $class->rectangles[10];
-//     $rec12 = $class->rectangles[11];
-//     if (count($class->occupants) == 1) {
-//         $per1 = $class->occupants[0];
-//     }
-//     elseif (count($class->occupants) == 2) {
-//         $per1 = $class->occupants[0];
-//         $per2 = $class->occupants[1];
-//     }
-//     elseif (count($class->occupants) == 3) {
-//         $per1 = $class->occupants[0];
-//         $per2 = $class->occupants[1];
-//         $per3 = $class->occupants[2];
-//     }
-//     elseif (count($class->occupants) == 4) {
-//         $per1 = $class->occupants[0];
-//         $per2 = $class->occupants[1];
-//         $per3 = $class->occupants[2];
-//         $per4 = $class->occupants[3];
-//     }
-//     elseif (count($class->occupants) == 5) {
-//         $per1 = $class->occupants[0];
-//         $per2 = $class->occupants[1];
-//         $per3 = $class->occupants[2];
-//         $per4 = $class->occupants[3];
-//         $per5 = $class->occupants[4];
-//     }
-//     elseif (count($class->occupants) == 6) {
-//         $per1 = $class->occupants[0];
-//         $per2 = $class->occupants[1];
-//         $per3 = $class->occupants[2];
-//         $per4 = $class->occupants[3];
-//         $per5 = $class->occupants[4];
-//         $per6 = $class->occupants[5];
-//     }
-// }
-
 // Fixes a PHP serialization issue
 // https://tommcfarlin.com/cast-a-php-standard-class-to-a-specific-type/
 function cast($instance, $className) {
